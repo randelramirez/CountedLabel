a0a8d00487fb72da4b91e4f2fb3576c9cd078784
randelramirez <randelramirez1@gmail.com>
2016-10-17 21:08:37 +0800

    Add project files.

---
+++ CountedLabel/CountedLabel/CountPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CountedLabel
+{
+    public partial class CountPage : ContentPage
+    {
+        public CountPage()
+        {
+            InitializeComponent();
+            int wordCount = countedLabel.WordCount;
+            wordCountLabel.Text = wordCount + " words";
+        }
+    }
+}
